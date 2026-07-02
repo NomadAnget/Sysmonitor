7222c0af3e53074f9771843c46984e49521f8698
NomadOS <128327353+hurcaguari@users.noreply.github.com>
2026-07-03 03:15:39 +0800

    CI(GitHub): Nuitka 构建改用 cmd 单行传参, 修复 pwsh 下 Windows 选项被吞 (图标/控制台/libs)

---
+++ SysMonitor.cmd
@@ -0,0 +1,7 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'SysMonitor.exe' with Python DLL being found.
+set PATH=c:\users\hurca\appdata\roaming\uv\python\cpython-3.13-windows-x86_64-none;%PATH%
+set PYTHONHOME=C:\Users\Hurca\AppData\Roaming\uv\python\cpython-3.13-windows-x86_64-none
+set NUITKA_PYTHONPATH=G:\claude_server\sysmonitor;C:\Users\Hurca\AppData\Roaming\uv\python\cpython-3.13-windows-x86_64-none\DLLs;C:\Users\Hurca\AppData\Roaming\uv\python\cpython-3.13-windows-x86_64-none\Lib;C:\Users\Hurca\AppData\Roaming\uv\python\cpython-3.13-windows-x86_64-none;G:\claude_server\sysmonitor\.venv;G:\claude_server\sysmonitor\.venv\Lib\site-packages;G:\claude_server\sysmonitor\.venv\Lib\site-packages\win32;G:\claude_server\sysmonitor\.venv\Lib\site-packages\win32\lib;G:\claude_server\sysmonitor\.venv\Lib\site-packages\pythonwin
+"%~dp0SysMonitor.exe" %*
